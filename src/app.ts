98b1956bb285e1057dfcc63378f747a194514fd9
Igor <kisel.igor@yahoo.com>
2024-02-15 12:48:53 +0530

    Input set up

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
-export class App{
-    run(){
-        console.log(
-            'Done'
-        );
-        
-    }
+import { PromptService } from "./core/prompt/prompt.servise"
+
+
+export class App {
+  async run() {
+    const res = await new PromptService().input<number>('Number', 'number')
+    console.log(res)
+  }
 }
 
 const app = new App()
